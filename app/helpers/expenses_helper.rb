6d10a6a452914de4d2fbc5f4f4ad214eb3cb1c0e
ramonsantos <ramonsantos.pe@gmail.com>
2020-02-21 19:36:45 -0300

    Improves Expense form

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -4,4 +4,12 @@ module ExpensesHelper
   def only_numbers_script
     "this.value=this.value.replace(/[^0-9.]/g,'');"
   end
+
+  def expense_date(expense)
+    (expense.date || Time.zone.today).to_s
+  end
+
+  def current_option(expense, attribute_list)
+    (expense.place_id || attribute_list.first)
+  end
 end
