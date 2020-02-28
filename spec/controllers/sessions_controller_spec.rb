6a93a5425b5660a7cc9f5be6422ae431e533c86f
ramonsantos <ramonsantos.pe@gmail.com>
2020-02-28 20:31:50 -0300

    Add Session controller tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,4 +3,29 @@
 require 'rails_helper'
 
 describe SessionsController, type: :controller do
+  let!(:user) { create(:user) }
+
+  before do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe '#create' do
+    it 'authenticates user correctly' do
+      post(:create, params: { user: { email: user.email, password: user.password, remember_me: '0' } })
+
+      expect(flash[:notice]).to be_nil
+      expect(subject.current_user).to eq(user)
+    end
+  end
+
+  describe '#destroy' do
+    it 'log out user correctly' do
+      subject.sign_in(user)
+
+      post(:destroy)
+
+      expect(flash[:notice]).to be_nil
+      expect(subject.current_user).to be_nil
+    end
+  end
 end
