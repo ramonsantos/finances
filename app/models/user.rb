35cfacd05575f02cae081294d0ec27a55fcc9dd1
ramonsantos <ramonsantos.pe@gmail.com>
2020-03-28 19:14:32 -0300

    Remove recovery password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :expenses,       dependent: :delete_all
   has_many :places,         dependent: :delete_all
