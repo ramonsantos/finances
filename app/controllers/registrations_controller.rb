82847f0e4bb2ea0bfbcbc11ffc5f98dc612328e1
ramonsantos <ramonsantos.pe@gmail.com>
2020-05-16 19:57:27 -0300

    Refactoring

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,17 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
         build_create_response(user)
       end
     else
-      place_error = {
-        name: {
-          status: :error,
-          message: 'não pode ficar vazio'
-        }
-      }
-
-      build_resource(user_params).validate
-      build_create_response(resource, place_error)
-
-      render(:new)
+      render_error_response
     end
 
     flash.clear
@@ -31,21 +21,41 @@ class RegistrationsController < Devise::RegistrationsController
 
   private
 
+  def render_error_response
+    build_resource(user_params).validate
+    build_create_response(resource, place_error)
+
+    render(:new)
+  end
+
   def build_create_response(user, place_error = {})
-    if user.errors.details.blank? && place_error.blank?
+    if create_with_success?(user, place_error)
       Place.create(name: place_name, user: resource)
     else
-      @fields_validation = place_error.tap do |hash|
-        messages = user.errors.messages
+      @fields_validation = build_fields_validation(user, place_error)
+    end
+  end
 
-        messages.keys.map do |key|
-          hash[key] = {
-            status: :error,
-            message: messages[key].first
-          }
-        end
+  def build_fields_validation(user, place_error)
+    place_error.tap do |hash|
+      messages = user.errors.messages
+
+      messages.keys.map do |key|
+        hash[key] = build_field_validation(:error, messages[key].first)
       end
     end
+  end
+
+  def create_with_success?(user, place_error)
+    user.errors.details.blank? && place_error.blank?
+  end
+
+  def place_error
+    { name: build_field_validation(:error, 'não pode ficar vazio') }
+  end
+
+  def build_field_validation(status, message)
+    { status: status, message: message }
   end
 
   def user_params
