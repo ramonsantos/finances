f9e003b6135e2baf949db18201c5cc6a7835b5f2
ramonsantos <ramonsantos.pe@gmail.com>
2020-05-21 19:42:23 -0300

    Add params to filter

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -3,4 +3,21 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  # User
+  :password,
+
+  # Expense
+  :description,
+  :amount,
+  :date,
+  :fixed,
+  :remark,
+
+  # Loan
+  :person,
+  :borrowed_amount,
+  :loan_date,
+  :received_amount,
+  :received_at
+]
